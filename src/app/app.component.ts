16a5276bc523eac4ee44caaa2d401c79d44daef5
Krishnadas N <mkkrishnadas25@gmail.com>
2024-01-23 19:17:45 +0530

    todo list completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { Item } from './item';
+import {ItemComponent} from './ITEMS/item.component'
+import { CommonModule } from '@angular/common'
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone:true,
+  imports: [ItemComponent,CommonModule],
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css', './ITEMS/item.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  errorMessage:string=''
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems:Item[] = [
+    { description: 'eat', done: true },
+  
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+  
+
+  addItem(description: string) {
+    if(!description.match(/[a-zA-Z0-9]+$/)){
+       this.errorMessage="Please enter a Valid task"
+    }else{
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+    this.errorMessage=""
+  }
+  }
+
+  remove(item: Item) {
+    console.log('AppComponent - Remove item:', item);
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
